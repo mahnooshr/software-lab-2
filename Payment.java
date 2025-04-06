e47ea0445b088f4828e466b7d9406701c22a982d
mahnooshr <74074818+mahnooshr@users.noreply.github.com>
2025-04-06 20:35:32 +0330

    Update Payment.java
    added paymentDetails

--- Payment.java
+++ Payment.java
@@ -5,11 +5,13 @@ public abstract class Payment {
     protected String currency;
     protected long transactionTime;
     protected Map<String, String> customerInfo;
+     protected Map<String, String> paymentDetails;
     
     public Payment(double amount, String currency, Map<String, String> customerInfo) {
         this.amount = amount;
         this.currency = currency;
         this.customerInfo = customerInfo;
+        this.paymentDetails = paymentDetails;
         this.transactionTime = System.currentTimeMillis();
     }
 
