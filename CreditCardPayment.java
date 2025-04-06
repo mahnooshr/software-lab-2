735cb451c5cfecfde509c55659f1ea49a61c6233
mahnooshr <74074818+mahnooshr@users.noreply.github.com>
2025-04-06 20:43:34 +0330

    Update CreditCardPayment.java
    corrected  paymentDetails

--- CreditCardPayment.java
+++ CreditCardPayment.java
@@ -5,18 +5,19 @@ public class CreditCardPayment extends Payment {
 
     private Map<String, String> config;
 
-    public CreditCardPayment(double amount, String currency, Map<String, String> customerInfo, Map<String, String> config) {
-        super(amount, currency, customerInfo);
+    public CreditCardPayment(double amount, String currency, Map<String, String> customerInfo,
+                         Map<String, String> paymentDetails, Map<String, String> config) {
+        super(amount, currency, customerInfo, paymentDetails);
         this.config = config;
     }
 
     @Override
-    public boolean validatePayment(Map<String, String> paymentDetails) {
-       return paymentDetails.containsKey("card_number") && paymentDetails.get("card_number").length() >= 12;
+    public boolean validatePayment() {
+       return this.paymentDetails.containsKey("card_number") && paymentDetails.get("card_number").length() >= 12;
     }
 
     @Override
-    public Map<String, String> processPayment(Map<String, String> paymentDetails) {
+    public Map<String, String> processPayment() {
         System.out.println("Connecting to Credit Card API at " + this.config.get("credit_card_endpoint"));
         String transactionId = "CC" + new Date().getTime();
         System.out.println("Processing credit card payment for " + this.customerInfo.get("name"));
