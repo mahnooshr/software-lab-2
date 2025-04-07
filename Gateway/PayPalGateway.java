6648ea644a8769063ccd8c0b997a5bd53a7dcce9
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2025-04-07 20:26:22 +0330

    implement paypalGateway

---
+++ Gateway/PayPalGateway.java
@@ -0,0 +1,39 @@
+import java.util.Date;
+import java.util.Map;
+
+public class PayPalGateway implements PaymentGateway{
+    
+    private Map<String, String> config;
+
+    public PayPalGateway(Map<String, String> config){
+        this.config = config;
+    }
+
+    public String processPayment(double amount, String currency, 
+                                      Map<String, String> customerInfo,
+                                      Map<String, String> paymentDetails){
+
+        String paymentEndpoint = paymentDetails.get("payment_endpoint");
+        String baseUrl = this.config.get("paypal_process_endpoint");
+        System.out.println("Connecting to PayPalGateway at " + baseUrl + paymentEndpoint);
+
+        String paymentType = paymentDetails.get("payment_type");
+        String transactionId = paymentType + new Date().getTime();
+        
+        System.out.println("Processing digital wallet payment for " + this.customerInfo.get("name"));
+        return transactionId;
+    }
+    
+    public void refundPayment(Map<String, String> paymentDetails, String transactionId){
+        String paymentEndpoint = paymentDetails.get("payment_endpoint");
+        String baseUrl = this.config.get("paypal_refund_endpoint");
+        System.out.println("Refund payment with transaction id" + transaction_id + " to PayPalGateway at " + baseUrl + paymentEndpoint);
+    }
+    
+    public Map<String, String> getTransactionStatus(String transactionId){
+        String paymentEndpoint = paymentDetails.get("payment_endpoint");
+        String baseUrl = this.config.get("paypal_get_status_endpoint");
+        System.out.println("Get status of payment with transaction id" + transaction_id + " to PayPalGateway at " + baseUrl + paymentEndpoint);
+        return Map.of("status", "success", "transaction_id", transactionId);
+    }
+}
--- Gateway/PaymentGateway.java
+++ Gateway/PaymentGateway.java
@@ -3,12 +3,12 @@ import java.util.Map;
 
 public interface PaymentGateway {
 
-    Map<String, String> processPayment(double amount, String currency, 
+    String processPayment(double amount, String currency, 
                                       Map<String, String> customerInfo,
                                       Map<String, String> paymentDetails);
     
  
-    Map<String, String> refundPayment(String transactionId);
+    void refundPayment(Map<String, String> paymentDetails, String transactionId);
     
   
     Map<String, String> getTransactionStatus(String transactionId);
